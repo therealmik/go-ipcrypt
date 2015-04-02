e79b1c6dd27cf9dbe4432ceda4485e6ba39f97c3
Michael Samuel <mik@miknet.net>
2015-04-02 13:09:59 +1100

    Almost working 32-bit vector version

--- ipcrypt.go
+++ ipcrypt.go
@@ -4,97 +4,165 @@ https://github.com/veorq/ipcrypt
 */
 package ipcrypt
 
-type Key [4][4]byte
+const (
+	B0 uint32 = 0xff
+	B1 uint32 = B0 << 8
+	B2 uint32 = B1 << 8
+	B3 uint32 = B2 << 8
+
+	B0B2 uint32 = B0 | B2
+	B1B3 uint32 = B1 | B3
+)
+
+type Key [4]uint32
 
 // Setup a key.  Will truncate or zero-pad to 16 bytes.
 func KeySetup(key []byte) Key {
 	var k Key
+	var tmp [4][4]byte
 
 	for i := 0; i < 16 && i < len(key); i++ {
-		k[i/4][i%4] = key[i]
+		tmp[i/4][i%4] = key[i]
+	}
+	for i := 0; i < 4; i++ {
+		k[i] = toState(tmp[i])
 	}
 
 	return k
 }
 
+func toState(ip [4]byte) uint32 {
+	return (uint32(ip[3]) << 24) | (uint32(ip[2]) << 16) | (uint32(ip[1]) << 8) | uint32(ip[0])
+}
+
+func fromState(s uint32) [4]byte {
+	return [4]byte{byte(s), byte(s >> 8), byte(s >> 16), byte(s >> 24)}
+}
+
 // Encrypt a 4-byte value
 func Encrypt(key Key, ip [4]byte) [4]byte {
-	s := state(ip)
-	s = xor4(s, key[0])
+	s := toState(ip)
+
+	s ^= key[0]
 	s = fwd(s)
-	s = xor4(s, key[1])
+	s ^= key[1]
 	s = fwd(s)
-	s = xor4(s, key[2])
+	s ^= key[2]
 	s = fwd(s)
-	s = xor4(s, key[3])
-	return s
+	s ^= key[3]
+
+	return fromState(s)
 }
 
 // Decrypt a 4-byte value with a 16-byte key
 func Decrypt(key Key, ip [4]byte) [4]byte {
-	s := state(ip)
-	s = xor4(s, key[3])
+	s := toState(ip)
+
+	s ^= key[3]
 	s = bwd(s)
-	s = xor4(s, key[2])
+	s ^= key[2]
 	s = bwd(s)
-	s = xor4(s, key[1])
+	s ^= key[1]
 	s = bwd(s)
-	s = xor4(s, key[0])
+	s ^= key[0]
+
+	return fromState(s)
+}
+
+func fwd(s uint32) uint32 {
+	// b0 += b1
+	// b2 += b3
+	// b1 = rotl(b1, 2)
+	// b3 = rotl(b3, 5)
+	// b1 ^= b0
+	// b3 ^= b2
+	// b0 = rotl(b0, 4)
+	// b0 += b3
+	// b2 += b1
+	// b1 = rotl(b1, 3)
+	// b3 = rotl(b3, 7)
+	// b1 ^= b2
+	// b3 ^= b0
+	// b2 = rotl(b2, 4)
+	s = add(s, s>>8)
+	s = rotlb1b3(s, 2, 5)
+	s = xor(s, s<<8)
+	s = rotl(s, 4, B0)
+	s = add(s, (s<<8)|(s>>24))
+	s = rotlb1b3(s, 3, 7)
+	s = xor(s, (s<<24)|(s>>8))
+	s = rotl(s, 4, B2)
+
 	return s
 }
 
-type state [4]byte
+func bwd(s uint32) uint32 {
+	// b2 = rotl(b2, 4)
+	// b1 ^= b2
+	// b3 ^= b0
+	// b1 = rotl(b1, 5)
+	// b3 = rotl(b3, 1)
+	// b2 -= b1
+	// b0 -= b3
+	// b0 = rotl(b0, 4)
+	// b1 ^= b0
+	// b3 ^= b2
+	// b1 = rotl(b1, 6)
+	// b3 = rotl(b3, 3)
+	// b0 -= b1
+	// b2 -= b3
 
-func fwd(s state) state {
-	b0, b1, b2, b3 := s[0], s[1], s[2], s[3]
-	b0 += b1
-	b2 += b3
-	b0 &= 0xff
-	b2 &= 0xff
-	b1 = rotl(b1, 2)
-	b3 = rotl(b3, 5)
-	b1 ^= b0
-	b3 ^= b2
-	b0 = rotl(b0, 4)
-	b0 += b3
-	b2 += b1
-	b0 &= 0xff
-	b2 &= 0xff
-	b1 = rotl(b1, 3)
-	b3 = rotl(b3, 7)
-	b1 ^= b2
-	b3 ^= b0
-	b2 = rotl(b2, 4)
-	return state{b0, b1, b2, b3}
+	s = rotl(s, 4, B2)
+	s = xor(s, (s<<24)|(s>>8))
+	s = rotlb1b3(s, 5, 1)
+	s = sub(s, (s<<8)|(s>>24))
+	s = rotl(s, 4, B0)
+	s = xor(s, s<<8)
+	s = rotlb1b3(s, 6, 3)
+	s = sub(s, s>>8)
+
+	return s
 }
 
-func bwd(s state) state {
-	b0, b1, b2, b3 := s[0], s[1], s[2], s[3]
-	b2 = rotl(b2, 4)
-	b1 ^= b2
-	b3 ^= b0
-	b1 = rotl(b1, 5)
-	b3 = rotl(b3, 1)
-	b0 -= b3
-	b2 -= b1
-	b0 &= 0xff
-	b2 &= 0xff
-	b0 = rotl(b0, 4)
-	b1 ^= b0
-	b3 ^= b2
-	b1 = rotl(b1, 6)
-	b3 = rotl(b3, 3)
-	b0 -= b1
-	b2 -= b3
-	b0 &= 0xff
-	b2 &= 0xff
-	return state{b0, b1, b2, b3}
+func rotlb1b3(b uint32, ra, rb uint) uint32 {
+	var unused uint32 = b & B0B2
+	var a uint32 = b & B1
+	b = b & B3
+
+	a = (a << ra) | (a >> (8 - ra))
+	a &= B1
+
+	b = (b << rb) | (b >> (8 - rb))
+	b &= B3
+
+	return a | b | unused
 }
 
-func rotl(b byte, r uint) byte {
-	return ((b << r) & 0xff) | (b >> (8 - r))
+func rotl(b uint32, r uint, mask uint32) uint32 {
+	var unused uint32 = b & (^mask)
+
+	b &= mask
+	b = (b << r) | (b >> (8 - r))
+	b &= mask
+
+	return b | unused
 }
 
-func xor4(x [4]byte, y [4]byte) [4]byte {
-	return [4]byte{x[0] ^ y[0], x[1] ^ y[1], x[2] ^ y[2], x[3] ^ y[3]}
+func add(a, b uint32) uint32 {
+	var unused uint32 = a & B1B3
+	a &= B0B2
+	b &= B0B2
+	return ((a + b) & B0B2) | unused
+}
+
+func sub(a, b uint32) uint32 {
+	var unused uint32 = a & B1B3
+	a &= B0B2
+	b &= B0B2
+	return ((a - b) & B0B2) | unused
+}
+
+func xor(a, b uint32) uint32 {
+	var unused uint32 = a & B0B2
+	return ((a ^ b) & B1B3) | unused
 }
--- ipcrypt_test.go
+++ ipcrypt_test.go
@@ -4,11 +4,23 @@ import (
 	"testing"
 
 	"github.com/dgryski/go-skip32"
+	"math/rand"
 )
+
+func TestFwdBwd(t *testing.T) {
+	for i := 0; i < 16; i++ {
+		s := rand.Uint32()
+		r := fwd(s)
+		q := bwd(r)
+		if q != s {
+			t.Errorf("fwd(%08x) = %08x; bwd(%08x) = %08x (diff=%08x)", s, r, r, q, s ^ q)
+		}
+	}
+}
 
 func TestRoundtrip(t *testing.T) {
 
-	key := Key{{0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}}
+	key := Key{0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff}
 
 	ip := [4]byte{1, 2, 3, 4}
 
@@ -17,7 +29,7 @@ func TestRoundtrip(t *testing.T) {
 	}
 
 	if ip != [4]byte{107, 47, 222, 186} {
-		t.Errorf("Encrypt failed")
+		t.Errorf("Encrypt failed, result %v", ip)
 	}
 
 	for i := 0; i < 100; i++ {
@@ -25,13 +37,13 @@ func TestRoundtrip(t *testing.T) {
 	}
 
 	if ip != [4]byte{1, 2, 3, 4} {
-		t.Errorf("Decrypt failed")
+		t.Errorf("Decrypt failed, result %v", ip)
 	}
 }
 
 func BenchmarkIPCrypt(b *testing.B) {
 
-	key := Key{{0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}}
+	key := Key{0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff}
 	ip := [4]byte{1, 2, 3, 4}
 
 	for i := 0; i < b.N; i++ {
@@ -48,5 +60,44 @@ func BenchmarkSkip32(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		s.Obfus(ip)
+	}
+}
+
+func TestROTL(t *testing.T) {
+	s := rotl(0x71ffffff, 4, B3)
+	if s != 0x17ffffff {
+		t.Errorf("rotl doesn't work: %x", s)
+	}
+
+	s = rotl(0xffff71ff, 4, B1)
+	if s != 0xffff17ff {
+		t.Errorf("rotl doesn't work: %x", s)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	var a uint32 = 0xff01ff01
+	var b uint32 = 0xffffffff
+	s := add(a, b)
+	if s != 0xff00ff00 {
+		t.Errorf("add doesn't work: %x", s)
+	}
+}
+
+func TestSub(t *testing.T) {
+	var a uint32 = 0xff0000ff
+	var b uint32 = 0x77011111
+	s := sub(a, b)
+	if s != 0xffff00ee {
+		t.Errorf("sub doesn't work: %x", s)
+	}
+}
+
+func TestXor(t *testing.T) {
+	var a uint32 = 0x55111111
+	var b uint32 = 0xcc772211
+	s := xor(a, b)
+	if s != 0x99113311 {
+		t.Errorf("xor doesn't work: %x", s)
 	}
 }
